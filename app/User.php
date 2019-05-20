8e57eb6aa19b4b4797682d682074a87511de0e5e
wayne <wjmpantig@gmail.com>
2019-05-20 09:53:14 +0800

    updated fillable attribute

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'last_name', 'first_name', 'email', 'password',
     ];
 
     protected $dates = [
